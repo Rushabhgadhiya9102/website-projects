5eca5a034db11bbe39c8ed77ab3f73a818d30594
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-05-31 18:37:53 +0530

    commited

--- admin-panel/scripts/dashboard.js
+++ admin-panel/scripts/dashboard.js
@@ -1,6 +1,184 @@
-$(' #analysis, #sales, #register').hide();
+// -------------------- S I D E B A R -----------------
 
-$('#sidebar>button').on('click',function(){
-    $('#dashboard, #sales, #register, #analysis').hide();
-   $($(this).attr("for")).show(); 
-})
+$("#side-bar").css("width","5%")
+$('.btnNameHide').hide();
+
+let sideBar = true;
+
+function sidebarCustomization(){
+    sideBar = !sideBar;
+    if(sideBar){
+      $("#side-bar").css("width","5%")
+      $('.btnNameHide').css("display","none");
+    }else{
+      $("#side-bar").css("width","20%")
+      $('.btnNameHide').css("display","block");
+    }
+}
+
+
+$('.bx-menu-alt-right').on('click',function(){
+  sidebarCustomization();
+   
+});
+
+
+// --------------- C H A R T S -----------------
+
+var options = {
+    series: [{
+    name: 'Inflation',
+    data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+  }],
+    chart: {
+    height: 350,
+    type: 'bar',
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 10,
+      dataLabels: {
+        position: 'top',
+      },
+    }
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val + "%";
+    },
+    offsetY: -20,
+    style: {
+      fontSize: '12px',
+      colors: ["#304758"]
+    }
+  },
+  
+  xaxis: {
+    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    position: 'top',
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false
+    },
+    crosshairs: {
+      fill: {
+        type: 'gradient',
+        gradient: {
+          colorFrom: '#D8E3F0',
+          colorTo: '#BED1E6',
+          stops: [0, 100],
+          opacityFrom: 0.4,
+          opacityTo: 0.5,
+        }
+      }
+    },
+    tooltip: {
+      enabled: true,
+    }
+  },
+  yaxis: {
+    axisBorder: {
+      show: false
+    },
+    axisTicks: {
+      show: false,
+    },
+    labels: {
+      show: false,
+      formatter: function (val) {
+        return val + "%";
+      }
+    }
+  
+  },
+  title: {
+    text: 'Monthly Inflation in Argentina, 2002',
+    floating: true,
+    offsetY: 330,
+    align: 'center',
+    style: {
+      color: '#444'
+    }
+  }
+  };
+  
+  var chart = new ApexCharts(document.querySelector("#chart1"), options);
+  chart.render();
+  
+  //   CHART-2  //
+  
+  var options = {
+    series: [{
+    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 4,
+      horizontal: true,
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  xaxis: {
+    categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
+      'United States', 'China', 'Germany'
+    ],
+  }
+  };
+  
+  var chart = new ApexCharts(document.querySelector("#chart2"), options);
+  chart.render();
+  
+
+  //chart-3//
+  var options = {
+          series: [{
+          name: 'Website Blog',
+          type: 'column',
+          data: [440, 505, 414, 671, 227, 413, 201, 352, 752, 320, 257, 160]
+        }, {
+          name: 'Social Media',
+          type: 'line',
+          data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
+        }],
+          chart: {
+          height: 350,
+          type: 'line',
+        },
+        stroke: {
+          width: [0, 4]
+        },
+        title: {
+          text: 'Traffic Sources'
+        },
+        dataLabels: {
+          enabled: true,
+          enabledOnSeries: [1]
+        },
+        labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
+        xaxis: {
+          type: 'datetime'
+        },
+        yaxis: [{
+          title: {
+            text: 'Website Blog',
+          },
+        
+        }, {
+          opposite: true,
+          title: {
+            text: 'Social Media'
+          }
+        }]
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart3"), options);
+        chart.render();  
