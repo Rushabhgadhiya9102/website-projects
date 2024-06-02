49b7e809834bf206f5bb2d82536fca4cd7b0c382
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-06-03 01:38:26 +0530

    done

---
+++ admin-panel/scripts/analysis.js
@@ -0,0 +1,461 @@
+
+// ------------------- C H A R T - 1 -----------------------
+
+
+var options = {
+    series: [
+    {
+      name: "High - 2013",
+      data: [28, 29, 33, 36, 32, 32, 33]
+    },
+    {
+      name: "Low - 2013",
+      data: [12, 11, 14, 18, 17, 13, 13]
+    }
+  ],
+    chart: {
+    height: 350,
+    type: 'line',
+    dropShadow: {
+      enabled: true,
+      color: '#000',
+      top: 18,
+      left: 7,
+      blur: 10,
+      opacity: 0.2
+    },
+    zoom: {
+      enabled: false
+    },
+    toolbar: {
+      show: false
+    }
+  },
+  colors: ['#77B6EA', '#545454'],
+  dataLabels: {
+    enabled: true,
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  title: {
+    text: 'Average High & Low Temperature',
+    align: 'left'
+  },
+  grid: {
+    borderColor: '#e7e7e7',
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  markers: {
+    size: 1
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    title: {
+      text: 'Month'
+    }
+  },
+  yaxis: {
+    title: {
+      text: 'Temperature'
+    },
+    min: 5,
+    max: 40
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'right',
+    floating: true,
+    offsetY: -25,
+    offsetX: -5
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#analysis-chart-1"), options);
+  chart.render();
+
+  // ------------------- C H A R T - 2 -----------------------
+
+  var options = {
+    series: [{
+      name: "Session Duration",
+      data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+    },
+    {
+      name: "Page Views",
+      data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
+    },
+    {
+      name: 'Total Visits',
+      data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
+    }
+  ],
+    chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: [5, 7, 5],
+    curve: 'straight',
+    dashArray: [0, 8, 5]
+  },
+  title: {
+    text: 'Page Statistics',
+    align: 'left'
+  },
+  legend: {
+    tooltipHoverFormatter: function(val, opts) {
+      return val + ' - <strong>' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + '</strong>'
+    }
+  },
+  markers: {
+    size: 0,
+    hover: {
+      sizeOffset: 6
+    }
+  },
+  xaxis: {
+    categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
+      '10 Jan', '11 Jan', '12 Jan'
+    ],
+  },
+  tooltip: {
+    y: [
+      {
+        title: {
+          formatter: function (val) {
+            return val + " (mins)"
+          }
+        }
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val + " per session"
+          }
+        }
+      },
+      {
+        title: {
+          formatter: function (val) {
+            return val;
+          }
+        }
+      }
+    ]
+  },
+  grid: {
+    borderColor: '#f1f1f1',
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#analysis-chart-2"), options);
+  chart.render();
+
+//   ----------------------------- C H A R T - 3 ------------------------------
+
+var options = {
+    series: [76, 67, 61, 90],
+    chart: {
+    height: 390,
+    type: 'radialBar',
+  },
+  plotOptions: {
+    radialBar: {
+      offsetY: 0,
+      startAngle: 0,
+      endAngle: 270,
+      hollow: {
+        margin: 5,
+        size: '30%',
+        background: 'transparent',
+        image: undefined,
+      },
+      dataLabels: {
+        name: {
+          show: false,
+        },
+        value: {
+          show: false,
+        }
+      },
+      barLabels: {
+        enabled: true,
+        useSeriesColors: true,
+        margin: 8,
+        fontSize: '16px',
+        formatter: function(seriesName, opts) {
+          return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex]
+        },
+      },
+    }
+  },
+  colors: ['#1ab7ea', '#0084ff', '#39539E', '#0077B5'],
+  labels: ['Vimeo', 'Messenger', 'Facebook', 'LinkedIn'],
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      legend: {
+          show: false
+      }
+    }
+  }]
+  };
+
+  var chart = new ApexCharts(document.querySelector("#analysis-chart-3"), options);
+  chart.render();
+
+
+//   ------------------------ C H A R T - 4 ----------------------------
+
+var options = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+    type: 'donut',
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+  };
+
+  var chart = new ApexCharts(document.querySelector("#analysis-chart-4"), options);
+  chart.render();
+
+//   ----------------------------- C H A R T - 5 ----------------------------------
+
+var options = {
+    series: [{
+    name: 'Marine Sprite',
+    data: [44, 55, 41, 37, 22, 43, 21]
+  }, {
+    name: 'Striking Calf',
+    data: [53, 32, 33, 52, 13, 43, 32]
+  }, {
+    name: 'Tank Picture',
+    data: [12, 17, 11, 9, 15, 11, 20]
+  }, {
+    name: 'Bucket Slope',
+    data: [9, 7, 5, 8, 6, 9, 4]
+  }, {
+    name: 'Reborn Kid',
+    data: [25, 12, 19, 32, 25, 24, 10]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350,
+    stacked: true,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      dataLabels: {
+        total: {
+          enabled: true,
+          offsetX: 0,
+          style: {
+            fontSize: '13px',
+            fontWeight: 900
+          }
+        }
+      }
+    },
+  },
+  stroke: {
+    width: 1,
+    colors: ['#fff']
+  },
+  title: {
+    text: 'Fiction Books Sales'
+  },
+  xaxis: {
+    categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+    labels: {
+      formatter: function (val) {
+        return val + "K"
+      }
+    }
+  },
+  yaxis: {
+    title: {
+      text: undefined
+    },
+  },
+  tooltip: {
+    y: {
+      formatter: function (val) {
+        return val + "K"
+      }
+    }
+  },
+  fill: {
+    opacity: 1
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'left',
+    offsetX: 40
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#analysis-chart-5"), options);
+  chart.render();
+
+
+//   ---------------------------  C H A R T - 6 ---------------------------------
+
+var options = {
+    series: [{
+    name: 'Net Profit',
+    data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+  }, {
+    name: 'Revenue',
+    data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+  }, {
+    name: 'Free Cash Flow',
+    data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: '55%',
+      endingShape: 'rounded'
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ['transparent']
+  },
+  xaxis: {
+    categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+  },
+  yaxis: {
+    title: {
+      text: '$ (thousands)'
+    }
+  },
+  fill: {
+    opacity: 1
+  },
+  tooltip: {
+    y: {
+      formatter: function (val) {
+        return "$ " + val + " thousands"
+      }
+    }
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#analysis-chart-6"), options);
+  chart.render();
+
+//   --------------------------- C H A R T - 7 --------------------------------
+
+var options = {
+    series: [
+    {
+      data: [
+        {
+          x: '2008',
+          y: [2800, 4500]
+        },
+        {
+          x: '2009',
+          y: [3200, 4100]
+        },
+        {
+          x: '2010',
+          y: [2950, 7800]
+        },
+        {
+          x: '2011',
+          y: [3000, 4600]
+        },
+        {
+          x: '2012',
+          y: [3500, 4100]
+        },
+        {
+          x: '2013',
+          y: [4500, 6500]
+        },
+        {
+          x: '2014',
+          y: [4100, 5600]
+        }
+      ]
+    }
+  ],
+    chart: {
+    height: 350,
+    type: 'rangeBar',
+    zoom: {
+      enabled: false
+    }
+  },
+  plotOptions: {
+    bar: {
+      isDumbbell: true,
+      columnWidth: 3,
+      dumbbellColors: [['#008FFB', '#00E396']]
+    }
+  },
+  legend: {
+    show: true,
+    showForSingleSeries: true,
+    position: 'top',
+    horizontalAlign: 'left',
+    customLegendItems: ['Product A', 'Product B']
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      type: 'vertical',
+      gradientToColors: ['#00E396'],
+      inverseColors: true,
+      stops: [0, 100]
+    }
+  },
+  grid: {
+    xaxis: {
+      lines: {
+        show: true
+      }
+    },
+    yaxis: {
+      lines: {
+        show: false
+      }
+    }
+  },
+  xaxis: {
+    tickPlacement: 'on'
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#analysis-chart-7"), options);
+  chart.render();
--- admin-panel/scripts/dashboard.js
+++ admin-panel/scripts/dashboard.js
@@ -22,19 +22,19 @@ $(".bx-menu").on("click", function () {
 
 //-------- N A V I G A T I O N - C H A N G E S ---------
 
-$("#user,#messages,#icons,#register").hide();
+$("#user,#messages,#analysis,#icons,#register").hide();
 
 // ----buttons----
 
 $(".nav-button>span").on("click", function () {
-  $("#dashboard,#user,#messages,#icons,#register").hide();
+  $("#dashboard,#user,#messages,#analysis,#icons,#register").hide();
   $($(this).attr("for")).show();
 });
 
 //----icons----
 
 $(".nav-button>i").on("click", function () {
-  $("#dashboard,#user,#icons,#register").hide();
+  $("#dashboard,#user,#messages,#analysis,#icons,#register").hide();
   $($(this).attr("for")).show();
 });
 
