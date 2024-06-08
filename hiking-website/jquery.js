30da9ca485effe485ca7882873735bc71cfd6e00
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-06-09 00:30:14 +0530

    commited

--- hiking-website/jquery.js
+++ hiking-website/jquery.js
@@ -1,26 +1,23 @@
 $(document).ready(function () {
 
-  const systemSettingDark = window.matchMedia("(prefers-color-scheme: light)");
-  $(".fa-moon").hide();
-
+  const systemSettingDark = window.matchMedia("(prefers-color-scheme: light)"); 
   if (systemSettingDark.matches) {
     $("body").addClass("dark-mode");
   } else {
     $("body").addClass("light-mode");
   }
+});
 
+$(".fa-moon").hide();
 
-  $(".fa-sun").on("click", function () {
-    $("body").addClass("dark-mode");
-    $("fa-moon").hide();
-    $("fa-sun").show();
-  });
+$(".fa-sun").on("click", function () {
+  $("body").addClass("dark-mode");
+  $("fa-moon").show();
+  $("fa-sun").hide();
+});
 
-  $(".fa-moon").on("click", function () {
-    $("body").removeClass("dark-mode");
-    $("fa-sun").hide();
-    $("fa-moon").show();
-  });
-
-
+$(".fa-moon").on("click", function () {
+  $("body").removeClass("dark-mode");
+  $("fa-sun").show();
+  $("fa-moon").hide();
 });
