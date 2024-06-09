87281c050dc975c5a0bf327cf8aa8c1d1bd865f2
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-06-09 23:36:12 +0530

    toggle theme

--- hiking-website/jquery.js
+++ hiking-website/jquery.js
@@ -1,23 +1,16 @@
+// ----------- T O G G L E - T H E M E ----------------
+
 $(document).ready(function () {
 
   const systemSettingDark = window.matchMedia("(prefers-color-scheme: light)"); 
   if (systemSettingDark.matches) {
-    $("body").addClass("dark-mode");
-  } else {
     $("body").addClass("light-mode");
+  } else {
+    $("body").addClass("dark-mode");
   }
+$(".slider").on("click",function(){
+   $("body").toggleClass("dark-mode");
+})
 });
 
-$(".fa-moon").hide();
-
-$(".fa-sun").on("click", function () {
-  $("body").addClass("dark-mode");
-  $("fa-moon").show();
-  $("fa-sun").hide();
-});
-
-$(".fa-moon").on("click", function () {
-  $("body").removeClass("dark-mode");
-  $("fa-sun").show();
-  $("fa-moon").hide();
-});
+ 
