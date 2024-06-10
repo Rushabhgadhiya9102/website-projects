c12ef6efd6d345c1b5bc33ca1d28bc2a16e28197
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-06-10 22:52:44 +0530

    commited

--- hiking-website/jquery.js
+++ hiking-website/jquery.js
@@ -2,15 +2,22 @@
 
 $(document).ready(function () {
 
+  $(".fa-sun").hide()
+  $(".fa-moon").hide()
+
+
   const systemSettingDark = window.matchMedia("(prefers-color-scheme: light)"); 
   if (systemSettingDark.matches) {
     $("body").addClass("light-mode");
+    $(".fa-sun").show()
   } else {
     $("body").addClass("dark-mode");
+    $(".fa-moon").show()
   }
 $(".slider").on("click",function(){
    $("body").toggleClass("dark-mode");
 })
 });
+
 
  
