f9d5010cef764ad27e1e1cf6ab56e986cbbb151d
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-02-23 23:19:04 +0530

    done

---
+++ backyard/script/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navigation = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      navigation.classList.add("scrolled");
+    } else {
+      navigation.classList.remove("scrolled");
+    }
+  });
+});
